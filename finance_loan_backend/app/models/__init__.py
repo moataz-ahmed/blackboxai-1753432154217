53c02f3ef648cee69d89106b4beaeba65068ad73
Moataz Ahmed <moataz.amahmood@gmail.com>
2025-07-26 19:56:52 +0300

    Add finance_loan_backend/app/models/__init__.py 1753549011582

---
+++ finance_loan_backend/app/models/__init__.py
@@ -0,0 +1,7 @@
+from .user import User
+from .bank import Bank
+from .company import Company
+from .loan import Loan
+from .credit_limit import CreditLimit
+
+__all__ = ["User", "Bank", "Company", "Loan", "CreditLimit"]
